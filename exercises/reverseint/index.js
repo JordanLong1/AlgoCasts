ded86d3ace896109a8f51b73865e547b21808934
Jordan <jordanleelong5@gmail.com>
2020-11-30 11:55:11 -0800

    solved reverseint problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// gotchas: we can turn a number into string using .toString()
+// Math.sign() pass in a number, if its positive itll return 1, if negative itll return -1
+//parseInt(MyNumber.toString()) convert string back into integer
+
+function reverseInt(n) {
+
+    let reversed = ''
+
+    n = n.toString()
+
+    for (let i = 0; i < n.length; i++ ) {
+        let current = n[i]
+
+        reversed = current + reversed
+       reversed = parseInt(reversed, 10)
+
+    }
+ 
+    if ( n < 0 ) {
+       return reversed * - 1
+    // return Math.sign(n) * reversed
+    }
+    
+    return reversed
+}
 
 module.exports = reverseInt;
